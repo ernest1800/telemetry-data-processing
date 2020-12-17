899c0ca43e47fd605f6b8e1d6a3fc5e0641f15e6
Ernest Obihara <ernest1800@protonmail.com>
2020-12-17 14:20:47 +0000

    implementing testing

---
+++ includes/telemetry/app/src/MonologWrapper.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Class for Logging using monolog
+ */
+
+namespace Telemetry;
+
+class Logger
+{
+    private $dbHandle;
+    private
+
+    public function __construct()
+    {
+    }
+
+    public function __destruct()
+    {
+    }
+
+    public function setDbHandle($db_handle)
+    {
+        $this->dbHandle = $this->dbHandle;
+    }
+
+    public function fetchDbConnectionResult()
+    {
+
+    }
+
+    public function storeLog()
+    {
+        return;
+    }
+}
---
+++ includes/telemetry/app/tests/XmlParserTest.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Telemetry;
+
+
+class XmlParserTest
+{
+
+}
