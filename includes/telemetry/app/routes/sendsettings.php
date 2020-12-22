d3529d1f5cb4aa72d1c4d5b7910d8aead88a5296
Ernest Obihara <ernest1800@protonmail.com>
2020-12-22 18:03:06 +0000

    adding send message functionality

---
+++ includes/telemetry/app/routes/sendsettings.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * home.php
+ */
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+//closure so route can have multiple names
+$home = function(Request $request, Response $response) use ($app)
+{
+
+    $sid = session_id();
+
+    $html_output = $this->view->render($response,
+        'home.html.twig',
+        [
+            'css_path' => CSS_PATH,
+            'landing_page' => LANDING_PAGE,
+            'action' => 'loadmessages',
+            'page_title' => APP_NAME,
+            'page_heading_1' => APP_NAME,
+            'page_heading_2' => 'Telemetry Homepage',
+            'button_text_load' => 'Load Telemetry Readings',
+            'button_text_send' => 'Send New Settings',
+            'load_messages_page' => 'displaymessages',
+            'send_settings_page' => 'sendsettings',
+
+        ]);
+
+    processOutput($app, $html_output);
+
+    return $html_output;
+
+};
+
+$app->get('/', $home)->setName('home');
+$app->get('/home', $home)->setName('home');
+
+function processOutput($app, $html_output)
+{
+    $process_output = $app->getContainer()->get('processOutput');
+    $html_output = $process_output->processOutput($html_output);
+    return $html_output;
+}
