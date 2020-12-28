9c4173902d22a459ea04aa1da6ee058e341e503c
Ernest Obihara <ernest1800@protonmail.com>
2020-12-28 13:47:07 +0000

    added vieew current settings and refactored sensor to switch

---
+++ includes/telemetry/app/routes/viewsettings.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * home.php
+ */
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+//closure so route can have multiple names
+$home = function(Request $request, Response $response) use ($app)
+{
+
+    $sid = session_id();
+
+    $html_output = $this->view->render($response,
+        'home.html.twig',
+        [
+            'css_path' => CSS_PATH,
+            'landing_page' => LANDING_PAGE,
+            'action' => 'loadmessages',
+            'page_title' => APP_NAME,
+            'page_heading_1' => APP_NAME,
+            'page_heading_2' => 'Telemetry Homepage',
+            'button_text_view' => 'View Telemetry',
+            'button_text_load' => 'View History',
+            'button_text_send' => 'Send New Settings',
+            'view_settings_page' => 'viewsettings',
+            'load_messages_page' => 'displaymessages',
+            'send_settings_page' => 'sendsettings',
+            'home_message' => 'Welcome to the Telemetry App...'
+
+        ]);
+
+    processOutput($app, $html_output);
+
+    return $html_output;
+
+};
+
+$app->get('/', $home)->setName('home');
+$app->get('/home', $home)->setName('home');
+
+function processOutput($app, $html_output)
+{
+    $process_output = $app->getContainer()->get('processOutput');
+    $html_output = $process_output->processOutput($html_output);
+    return $html_output;
+}
